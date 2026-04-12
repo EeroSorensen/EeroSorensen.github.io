e92d9707ae402f4f8e047f79cda5462e60d8e5bc
EeroSorensen <eeroriv@gmail.com>
2026-04-12 18:23:24 +0000

    Deploying to gh-pages from @ EeroSorensen/EeroSorensen.github.io@09da06b38492412e0281aa1c86bf980e2e166706 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Kinesiology student at the University of Calgary with interest in adapted physical activity and data analysis",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";

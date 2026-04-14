ca2abdad4350271f3710edcdc67145311cc6c42a
EeroSorensen <eeroriv@gmail.com>
2026-04-14 18:52:18 +0000

    Deploying to gh-pages from @ EeroSorensen/EeroSorensen.github.io@48ce7d8ffd57a5d990e9eb9a6310e646cede7a0d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -439,7 +439,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/EeroSResume", "_blank");
+          window.open("/assets/pdf/EeroSResume.pdf", "_blank");
         },
       },{
         id: 'social-email',
